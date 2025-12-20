65b9d95ebf352f63476b87725c897250c8f2840b
Kacper Mazurek <kacper030904@gmail.com>
2025-12-20 23:22:09 +0100

    moved nestJS to backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -9,21 +9,18 @@ import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      // Ścieżka do folderu 'dist' Twojego Reacta
-      // Zakładając, że masz strukturę: /frontend i /backend obok siebie
-      rootPath: join(__dirname, '..', '..', 'frontend', 'dist'), 
-      
-      // Wykluczamy ścieżkę API, żeby Nest nie szukał tam plików Reacta
-      exclude: ['/api/(.*)'], 
+      rootPath: join(__dirname, '..', '..', 'frontend', 'dist')
     }),
     ConfigModule.forRoot({isGlobal: true, }),
     AuthModule,
     UsersModule,
     DatabaseModule,
+    ProductsModule
   ],
   controllers: [AppController],
   providers: [AppService, DatabaseService],
