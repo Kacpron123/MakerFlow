d42dbf71f4a5ad6b047a49cf8970fd076cb17b38
Kacpron123 <kacper030904@gmail.com>
2025-12-31 18:05:23 +0100

    refactor: remove unnessary useToast hook

--- init.sql
+++ init.sql
@@ -10,6 +10,7 @@ CREATE TABLE groups (
    group_id SERIAL PRIMARY KEY,
    user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
    name TEXT NOT NULL,
+   stock_number INT DEFAULT 0,
    parent_group_id INT REFERENCES groups(group_id) ON DELETE SET NULL,
    description TEXT
    );
