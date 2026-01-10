0d5b42b00e205e39892df3f5e1c18c1a94ea1f02
Kacpron123 <kacper030904@gmail.com>
2026-01-10 02:35:44 +0100

    feat: develop docker feat: implement registration form

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({mode}) => { return{
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
@@ -14,10 +14,19 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  server:{
+  server: mode==='development' ? {
+    host: '0.0.0.0',
+    port: 5173,
     strictPort: true,
     watch:{
-      ignored: ['**/node_modules/**']
+      ignored: ['**/node_modules/**'],
+      usePolling: true,
     },
-  },
-})
+    proxy: {'/api': {
+      target: 'http://backend:3000',
+      changeOrigin: true,
+      secure: false,
+      // rewrite: (path) => path.replace(/^\/api/, '/api'),
+    },},
+  } : {},
+}})
